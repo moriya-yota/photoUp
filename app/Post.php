59e46d3a7e096af83c9a0fdbec5296168f39feb3
moriya-yota <moriya-y@i-hearts.jp>
2020-06-25 21:22:51 +0900

    feature/58
    格言画面へ飛ぶように修正

---
+++ app/Post.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+class Post extends Model
+{
+//後にcreate()メソッドで保存するカラムを指定
+    protected $fillable = [
+        'image_file_name', 'image_title',
+    ];
+}
---
+++ database/migrations/2020_06_24_114456__create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image_file_name',100);//追加
+            $table->string('image_title',100);//追加
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
